025a036516abbb85298ff9eb2b33a274778a097c
Ahmad741-art <155184460+Ahmad741-art@users.noreply.github.com>
2024-12-04 00:39:19 +0100

    php code

---
+++ src/exercise01/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lektion 1 - Hello World</title>
+</head>
+<body>
+    <h1>Lektion 1 - Hello World</h1>
+
+    <?
+    // Vår första kod i PHP
+    echo 'Hello, World';
+    ?>
+
+    ____________________
+
+<?php
+$tal1 = 234;
+$tal2 = 923;
+
+$produkt = $tal1 * $tal2;
+
+echo "Produkten av $tal1 och $tal2 är: $produkt";
+?>
+</body>
+</html>
